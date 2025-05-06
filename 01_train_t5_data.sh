8e2b1ca9300342334dc1b1b47cded88ab7ddfdae
quyenquyenquyenquyen <nmq1123@gmail.com>
2025-05-06 23:53:33 +0700

    Update 01_train_t5_data.sh

--- 01_train_t5_data.sh
+++ 01_train_t5_data.sh
@@ -20,7 +20,7 @@ cp ${CKPT_SRC}/checkpoint.pt ${OUT}/checkpoint-epoch-5/
 CUDA_VISIBLE_DEVICES=0 python run_gen.py \
     --do_train --do_eval --do_eval_bleu \
     --task refine --sub_task small --model_type codet5 \
-    --data_num -1 --num_train_epochs 10 --warmup_steps 500 \
+    --data_num -1 --num_train_epochs 15 --warmup_steps 500 \
     --learning_rate 5e-5 --patience 3 --beam_size 5 \
     --gradient_accumulation_steps 1 \
     --tokenizer_name=Salesforce/codet5-base \
